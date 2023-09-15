9fa1469ae01831cc55d3f64b24cf4d567e70b01f
darbelaezm <143208551+darbelaezm@users.noreply.github.com>
2023-09-14 21:51:23 -0500

    Add files via upload

---
+++ pooepr19/POOEPR19.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pooepr19;
+
+public class POOEPR19 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
