a1a7ab5a16f4b0fc15cc42bf5cf77bab6bc6b108
darbelaezm <143208551+darbelaezm@users.noreply.github.com>
2023-09-17 20:30:34 -0500

    Add files via upload

---
+++ pooeres10/POOERES10.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pooeres10;
+
+
+public class POOERES10 {
+
+    public static void main(String[] args) {
+       Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
